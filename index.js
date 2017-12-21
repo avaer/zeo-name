9edb3cbdda50176fa824696d95b37e17047f68e5
Adrian Biedrzycki <a@modules.io>
2017-12-20 22:17:20 -0800

    Use deterministic hash naming

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const murmur = require('murmurhash');
+
 const adjectives = require('./adjectives.json');
 const animals = require('./animals.json');
 
 const _capitalize = s => s.charAt(0).toUpperCase() + s.slice(1);
 
-module.exports = () => {
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const animal = animals[Math.floor(Math.random() * animals.length)];
+module.exports = (s = String(Math.random())) => {
+  const adjective = adjectives[Math.floor(murmur(s + ':adjective') / 0xFFFFFFFF  * adjectives.length)];
+  const animal = animals[Math.floor(murmur(s + ':adjective') / 0xFFFFFFFF * animals.length)];
   return _capitalize(adjective) + ' ' + _capitalize(animal);
 };
