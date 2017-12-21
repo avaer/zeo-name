c071119773484eb8f0489edda313a0a81381ba77
Adrian Biedrzycki <a@modules.io>
2017-12-20 23:07:38 -0800

    Do not use arrow functions for cleaner bundling

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const murmur = require('murmurhash');
 const adjectives = require('./adjectives.json');
 const animals = require('./animals.json');
 
-const _capitalize = s => s.charAt(0).toUpperCase() + s.slice(1);
+function _capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
 
-module.exports = (s = String(Math.random())) => {
+module.exports = function(s = String(Math.random())) {
   const adjective = adjectives[Math.floor(murmur(s + ':adjective') / 0xFFFFFFFF  * adjectives.length)];
   const animal = animals[Math.floor(murmur(s + ':adjective') / 0xFFFFFFFF * animals.length)];
   return _capitalize(adjective) + ' ' + _capitalize(animal);
